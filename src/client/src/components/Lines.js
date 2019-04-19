80b196bc16aa89f773222f461407f5258bcd182a
Amber Sharma <sharma.amber.p@gmail.com>
2019-04-18 21:30:50 -0700

    Refactored into stateful component. Data still static.

--- src/client/src/components/Lines.js
+++ src/client/src/components/Lines.js
@@ -1,33 +1,41 @@
 import React from 'react';
 
-const Lines = () => {
-  return (
-    <div className="lines-view">
-      <div className="selections">
-        Choose a line:
-        <select>
-          <option>Hardcoded Line A</option>
-          <option>Hardcoded Line B</option>
-          <option>Hardcoded Line C</option>
-          <option>Hardcoded Line D</option>
-          <option>Hardcoded Line E</option>
-        </select>
-      </div>
-      <div className="lines-stop-list">
-        <ul>
-          <li>Hardcoded Stop 1</li>
-          <li>Hardcoded Stop 2</li>
-          <li>Hardcoded Stop 3</li>
-          <li>Hardcoded Stop 4</li>
-          <li>Hardcoded Stop 5</li>
-          <li>Hardcoded Stop 6</li>
-          <li>Hardcoded Stop 7</li>
-          <li>Hardcoded Stop 8</li>
-          <li>Hardcoded Stop 9</li>
-        </ul>
-      </div>
-    </div>
-  );
+class Lines extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            lines: {
+                "A": ["Nob Hill", "Japan Town"],
+                "B": ["Protrero Hill", "Financial District"],
+                "C": ["Beacon Hill", "Twin Peaks"]
+            },
+            current_line: "A",
+        }
+
+        this.setCurrentLine = this.setCurrentLine.bind(this);
+    }
+
+    setCurrentLine(line) {
+        this.setState({current_line: line});
+    }
+
+    render() {
+        return (
+            <div className="lines-view">
+              <div className="selections">
+                Choose a line:
+                <select onChange={(event) => this.setCurrentLine(event.target.value)}>
+                  {Object.keys(this.state.lines).map(line => <option>{`${line}`}</option>)}
+                </select>
+              </div>
+              <div className="lines-stop-list">
+                <ul>
+                  {this.state.lines[this.state.current_line].map(stop => <li>{`${stop}`}</li>)}
+                </ul>
+              </div>
+            </div>
+        );
+    }
 }
 
 export default Lines;
