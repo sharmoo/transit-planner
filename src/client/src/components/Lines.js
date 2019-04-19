332534e58725b3e095808db6cfc514dce8edf22b
Amber Sharma <sharma.amber.p@gmail.com>
2019-04-19 00:11:09 -0700

    Refactored Lines to use fetch api. Set up express endpoint for Lines (serving static data for now). Added proxy configuration in package.json to send any unknown endpoints to the express server.

--- src/client/src/components/Lines.js
+++ src/client/src/components/Lines.js
@@ -4,19 +4,26 @@ class Lines extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            lines: {
-                "A": ["Nob Hill", "Japan Town"],
-                "B": ["Protrero Hill", "Financial District"],
-                "C": ["Beacon Hill", "Twin Peaks"]
-            },
-            current_line: "A",
+            lines: {"": []},
+            currentLine: "",
         }
-
         this.setCurrentLine = this.setCurrentLine.bind(this);
     }
 
+    componentDidMount() {
+        fetch("/api/v1/lines")
+        .then(res => {
+            return res.json();
+        })
+        .then(json => {
+            this.setState({lines: json, currentLine: Object.keys(json)[0]})
+        }).catch(err => {
+            console.error(err);
+        })
+    }
+
     setCurrentLine(line) {
-        this.setState({current_line: line});
+        this.setState({currentLine: line});
     }
 
     render() {
@@ -25,12 +32,12 @@ class Lines extends React.Component {
               <div className="selections">
                 Choose a line:
                 <select onChange={(event) => this.setCurrentLine(event.target.value)}>
-                  {Object.keys(this.state.lines).map(line => <option>{`${line}`}</option>)}
+                  {Object.keys(this.state.lines).map((line, index) => <option key={index}>{`${line}`}</option>)}
                 </select>
               </div>
               <div className="lines-stop-list">
                 <ul>
-                  {this.state.lines[this.state.current_line].map(stop => <li>{`${stop}`}</li>)}
+                  {this.state.lines[this.state.currentLine].map((stop, index) => <li key={index}>{`${stop}`}</li>)}
                 </ul>
               </div>
             </div>
---
+++ src/server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express()
+const api = express.Router();
+
+const STATIC_LINES_DATA = {
+    "A": ["Nob Hill", "Japan Town"],
+    "B": ["Protrero Hill", "Financial District"],
+    "C": ["Beacon Hill", "Twin Peaks"]
+}
+
+// get request for translit line data
+// TODO: make this read from DB
+api.get("/lines", (req, res) => {
+    res.status(200).send(STATIC_LINES_DATA)
+});
+
+// mount all the api routes at /api/v1/{lines, ...}
+app.use("/api/v1", api);
+
+// check to make sure we've specified a port to listen on
+const PORT = process.env.PORT
+if(!process.env.PORT) {
+  const errMsg = "PORT environment variable is not defined"
+  console.error(errMsg)
+  throw new Error(errMsg)
+}
+
+// now listen on it
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+})
